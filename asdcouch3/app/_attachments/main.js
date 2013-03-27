c103211aa8400f2652184e92e1bf8668b1cf11d1
Mark McAninch <awacs68@fullsail.udu>
2013-03-26 23:07:50 -0400

    Update the repo

--- asdcouch3/app/_attachments/main.js
+++ asdcouch3/app/_attachments/main.js
@@ -1,4 +1,4 @@
-$('#loaddatapage').live("pageshow", function(){
+$('#loaddatapage').on("pageshow", function(){
     $.couch.db("asd-3").view("asd-3app/owners",{
         "success": function(data){
             $('#loaddatapageitems').empty();
@@ -6,16 +6,20 @@ $('#loaddatapage').live("pageshow", function(){
                 var docId = owner.value.key;
                 var docRev = owner.value.rev;
                 var name = owner.value.name;
-                $('#loaddatapageitems').append(
+            $('#loaddatapageitems').append(
                     $('<li>').append(
                         $('<a>').attr("href", "owner.html?owner=" + docId + "&docRev=" + docRev)
                             .text(name)
-                             
+                           
                       )
                   );
              });
              $('#dataloadingitems').listview('refresh'); 
-        }
+        },
+        "error" : function(error, parseerror){
+console.log('Error!' + error);
+console.log('Error!' + parseerror);
+}
     });
   });
-
+ 
